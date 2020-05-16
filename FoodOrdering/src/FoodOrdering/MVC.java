f4cf2d264e8d53e3a3b43c720258f77fd31f8dda
iffatuzzahra <54696540+iffatuzzahra@users.noreply.github.com>
2020-05-16 08:55:09 +0700

    Update MVC.java

--- FoodOrdering/src/FoodOrdering/MVC.java
+++ FoodOrdering/src/FoodOrdering/MVC.java
@@ -1,8 +1,6 @@
 package FoodOrdering;
 
 public class MVC {
-    //View vieww;
-    //JFrame currentFrame = new JFrame();
     View view = new View();
     Model model = new Model();
     Controller controller = new Controller(model,view);
